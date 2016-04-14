d8b552ea4b559f20ce2e9cc8635b2b603c4e6ced
beaulirl <beaulirl@gmail.com>
2016-04-14 18:11:15 +0300

    add new dish formating

--- Dish.php
+++ Dish.php
@@ -4,79 +4,79 @@
 */
 class Dish
 {
-	const FIRST_DISH = "1";
-	const SECOND_DISH = "2";
-	const SALAD = "3";
-	const DESERT = "4";
+    const FIRST_DISH = "1";
+    const SECOND_DISH = "2";
+    const SALAD = "3";
+    const DESERT = "4";
 
     /**
     *
     * @var string 
     */
-	protected $table; 
+    protected $table; 
 
     /**
     *
     * @var id 
     */
-	protected $id;
+    protected $id;
 
     /**
     *
     * @var string 
     */
-	protected $name;
+    protected $name;
 
     /**
-     * Create a new connection.
-     *
-     * @param object $connection 
-     */
-	function __construct( MySqlConnection $connection)
-	{
-		$this->table = $connection->getTableName();
-		$this->pdo = $connection->GetPdo();
-	}
+    * Create a new connection.
+    *
+    * @param object $connection 
+    */
+    function __construct( MySqlConnection $connection)
+    {
+        $this->table = $connection->getTableName();
+        $this->pdo = $connection->GetPdo();
+    }
 
     /**
-	* Create a new Dish
-	* 
-	* @param string $name
+    * Create a new Dish
+    * 
+    * @param string $name
     * @param string $type
     * @param string $recept
     */
-	public function createDish($name, $type, $recept)
-	{
-	    $stmt = $this->pdo->prepare("INSERT into ".$this->table." (name, type, recept) values (:name, :type, :recept)");
+    public function createDish($name, $type, $recept)
+    {
+        $stmt = $this->pdo->prepare("INSERT into ".$this->table." (name, type, recept) values (:name, :type, :recept)");
         $stmt->bindParam(':name', $name);
         $stmt->bindParam(':type', $type);
         $stmt->bindParam(':recept', $recept);
         $stmt->execute();
         $this->name = $name;
-	}
+    }
 
     /**
-	* Get random Dish by type
-	*
-	* @param string $type
-	* @return int
+    * Get random Dish by type
+    *
+    * @param string $type
+    * @return int
     */
-	public function getRandDish($type)
-	{ 
+    public function getRandDish($type)
+    { 
         $stmt = $this->pdo->query("SELECT name, recept, price from ".$this->table." where type='".$type."' order by RAND() limit 1");
-	    $row = $stmt->fetch();
-	    return $row;  
-	}
+        $row = $stmt->fetch();
+        return $row;  
+    }
 
     /**
-	* Get id Dish
-	*
-	* @return int
+    * Get id Dish
+    *
+    * @return int
     */
-	public function getIdDish()
-	{ 
+    public function getIdDish()
+    { 
         $stdt = $this->pdo->query("SELECT id from ".$this->table." where name='".$this->name."' limit 1");
         $this->id = $stdt->fetch();
         return $this->id['id'];
-	}
+    }
 }
