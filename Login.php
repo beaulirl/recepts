55affdb66490635e2d1bea0e3af3d7151ea59fec
beaulirl <beaulirl@gmail.com>
2016-06-06 21:03:34 +0300

    html changes

--- Login.php
+++ Login.php
@@ -1,7 +1,18 @@
-<form action="Login.php" method="post">           
-<h2>Login</h2>
-<input type="text" id="email" class="form" placeholder="E-mail" name="e_mail"> 
-<input type="password" id="passw" class="form" placeholder="Password" name="pasw"> 
-<button type="submit" class="btn" name="login" >Login</button>                        
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<form role="form" class="form-horisontal" action="Login.php" method="post">
+<div class="container"> 
+<h2>Авторизация</h2>       
+<div class="form-group">	
+  <label class="sr-only" for="exampleInputEmail1">Email</label>
+  <input type="text" id="email" class="form-control" placeholder="E-mail" name="e_mail">
+</div>
+<div class="form-group">	
+  <label class="sr-only" for="exampleInputPassword1">Пароль</label>	
+  <input type="password" id="passw" class="form-control" placeholder="Password" name="pasw">
+</div>
+<div class="form-group">
+   <button type="submit" class="btn btn-info" name="login" >Войти</button>   
+</div>                     
 </form>
+</div>  
 <?php require_once __DIR__ .'/vendor/autoload.php'; ?>
--- index.php
+++ index.php
@@ -1,49 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Recepts</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+  </head>
+  <body>
 <?php
 require_once __DIR__ .'/vendor/autoload.php';?>
+  <div class="container-fluid">
+   <div class="row">
+    <?php
+    if (isset($_SESSION["mail"])) {
+    ?>
+   </div>
+  </div>
+  <nav class="navbar navbar-default" role="navigation">
+   <div class="container-fluid">
+    <div class="navbar-header">   
+     <a href="/?out=1" class="btn btn-default navbar-btn navbar-right">Выход</a>
+     <p class="navbar-text">Вы зашли как <?php echo $_SESSION["mail"]; ?></p>
+    </div> 
+   </div>
+  </nav>
+  <div class="container">
+   <div class="row">
+    <div class="col-md-4">
+     <h2>Какие блюда вы готовите?</h2>
+      <form action="index.php" method="post" >           
+       <div class="input-group input-group-lg">
+        <input type="text" class="form-control" placeholder="Название" name="name">
+       </div>
+  <hr>
+       <div class="form-group">    
+        <select class="form-control" multiple name="type">
+         <option disabled>Выберите тип</option>
+         <?php $types = require('dish_type_dict.php');
+         foreach($types as $type => $v): ?>
+         <option value="<?php echo $type; ?>"><?= $v ?></option>
+         <?php endforeach; ?>
+        </select>
+       </div>
+  <hr>
+       <div class="input-group">
+        <input type="text" class="form-control" id="recept" placeholder="Ссылка на рецепт" name="recept">
+       </div>
+  <hr>
+       <div class="input-group col-md-4">
+        <input class="form-control" id="ingredient" type="text" placeholder="Ингредиент" name="ingredient"></li>
+        <input class="form-control" id="fats" type="text" placeholder="Жирность" name="fats"></li>
+        <input class="form-control" id="quantity" type="text" placeholder="Количество" name="quantity"></li>
+        <input class="form-control" id="price" type="text" placeholder="Цена" name="price"></li>
+       </div>
+            <button class="btn btn-default" type="submit" name="send">Отправить</button>           
+    </div>
+    <div class="col-md-4">
+     <h2>Что приготовить?</h2>
+     <button type="submit" id="week" class="btn btn-success btn-lg" name="msg" >На этой неделе</button>
+     <hr>
+     <div class="row">
+      <p><?php if (isset($form_string)) echo $form_string; ?></p>
+     </div>
+    </div>
+      </form> 
+    <div class="col-md-4">
+     <h2>Все мои рецепты:</h2>
+     <?php if (isset($recepts_array)) { $recepts = $recepts_array;
+     foreach($recepts as $type): ?>
+     <p><?php echo $type; ?></p>
+     <?php endforeach; } ?>
+    </div>
+   </div>
+            
 <?php
-
-if(isset($_SESSION["mail"])) 
-    {
-        echo "Вы зашли как ".$_SESSION["mail"];
-    
-?>
-<a href="/?out=1">Выход</a>
-<form action="index.php" method="post" >           
-<h2>Какие блюда вы готовите?</h2>
-<input id="name" type="text" placeholder="Название" name="name">
- <p><select  multiple name="type">
-    <option disabled>Выберите тип</option>
-    <?php $types = require('dish_type_dict.php');
-    foreach($types as $type => $v): ?>
-    <option value="<?php echo $type; ?>"><?= $v ?></option>
-    <?php endforeach; ?>
-   </select></p>
-<input id="recept" type="text" placeholder="Ссылка на рецепт" name="recept">
-<ul class="posts">
-<li><input id="ingredient" type="text" placeholder="Ингредиент" name="ingredient"></li>
-<li><input id="fats" type="text" placeholder="Жирность" name="fats"></li>
-<li><input id="quantity" type="text" placeholder="Количество" name="quantity"></li>
-<li><input id="price" type="text" placeholder="Цена" name="price"></li>
-
-</ul>
-
-<button type="submit" name="send">Отправить</button>                        
-<button type="submit" name="msg">На этой неделе</button>
-</form>
-<?php
-if(isset($_GET['out'])&&$_GET['out']==1)
- {
+if (isset($_GET['out']) && $_GET['out'] == 1) {
+   header('Location: index.php/out=1');
    session_unset();
+   header('Location: index.php');
  }   
- } 
+} 
  else {
 ?>
-<form action="Registration.php">
-    <input type="submit" name="reg" value="Регистрация">
-</form>
-<form action="Login.php" method="post">
-    <input type="submit" name="log" value="Авторизация">
-</form>
-<?php } ?>
 
+<form role="form" action="Registration.php" method="post">
+<div class="form-group">    
+  <input class="btn btn-lg btn-success" type="submit" name="reg" value="Регистрация"> 
+</div>
+</form>
+<form role="form" action="Login.php" method="post">
+<div class="form-group">   
+  <input class="btn btn-lg btn-primary" type="submit" name="log" value="Авторизация">
+</div>  
+</form>
+
+<?php } ?>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="app.js"></script>
+  </body>
+</html>
 
